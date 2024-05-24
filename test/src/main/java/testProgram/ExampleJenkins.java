67144947798dcda42851757bd2b6f68f6b47dc66
ANUPAMA VISHNU <ANUPAMA VISHNU@LAPTOP-QEKB9P62>
2024-05-24 18:45:37 +0400

    Commit1

---
+++ test/src/main/java/testProgram/ExampleJenkins.java
@@ -0,0 +1,13 @@
+package testProgram;
+
+public class ExampleJenkins {
+public void print() {
+	System.out.println("Hello World");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ExampleJenkins ex=new ExampleJenkins();
+ex.print();
+}
+
+}
